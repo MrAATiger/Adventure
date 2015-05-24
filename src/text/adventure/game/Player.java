124da517ef20ceb9f2b37ecbcd59d2d50b60faa8
Ludwig Biermann <ludi.biermann@gmail.com>
2015-05-24 17:46:08 +0200

    formatter and add toString in player

--- src/text/adventure/game/Player.java
+++ src/text/adventure/game/Player.java
@@ -2,42 +2,37 @@ package text.adventure.game;
 
 import java.util.Random;
 
-
 public class Player {
+
+	private int maxPlayerHealth = 100;
+	private static final int MAX_PLAYER_DEFENCE = 20;
+	private static final int MAX_PLAYER_ATTACK = 50;
+
+	private String playerName;
+
+	Random rand = new Random();
+
+	public Player(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public int playerDefence, playerAttack, dropChance, heal;
+
+	public void setMAX_PLAYER_HEALTH(int maxPlayerHealth) {
+		this.maxPlayerHealth = maxPlayerHealth;
+	}
+
+	public int getMAX_PLAYER_HEALTH() {
+		return maxPlayerHealth;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
 	
-		private int maxPlayerHealth = 						100;
-		private static final int MAX_PLAYER_DEFENCE =		 20;
-		private static final int MAX_PLAYER_ATTACK =		 50;
-		
-		private String playerName;
-		
-		Random rand = new Random ();
-		
-		public Player(String playerName) {
-			this.playerName = playerName;
-		}
-		
-		public int  playerDefence, playerAttack, dropChance, heal;
-		
-		public void setMAX_PLAYER_HEALTH (int maxPlayerHealth) {
-			this.maxPlayerHealth = maxPlayerHealth;
-		}
-		
-		public int getMAX_PLAYER_HEALTH () {
-			return maxPlayerHealth;
-		}
-	
-		public String getPlayerName (){
-			return playerName ;
-		}
-	
-		
-		
-		
-		}
-		
-		
+	@Override
+	public String toString(){
+		return playerName;
+	}
 
-
-
-
+}
