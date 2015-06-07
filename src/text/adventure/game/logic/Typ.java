618b80f0f4c3c17953afb863a657bdaefdabf9f2
MrAATiger <alex.angeloussis@web.de>
2015-06-07 15:38:16 +0200

    delete commentary

--- src/text/adventure/game/logic/Typ.java
+++ src/text/adventure/game/logic/Typ.java
@@ -9,32 +9,16 @@ public enum Typ {
 	
 	public static final int SEHR_EFFEKTIV = 100;  
 	
-	// sobald ein Angriff sehr effektiv ist, wird ein zusätzlicher Schade berechnet
-
-	// die Typen als String mit if? 
 	/*
-	 * 
-	 * => besser Lösung wäre es zunächst den Gegnern einen Typ zuzuordnen.
-	 * Feuerdrache.equals(Feuer)
-	 * Syntax?! => Obejkt erstellen von Feuer feuer = new Feuer (); 
-	 * 
-	 * in den einzelnen Klassen schreiben was effektiv ist?
-	 * 
-	 * Feuerdrache = Feuer
-	 * Syntax wäre gegeben. Würde den Wert annehmen.
-	 * Schwächen in den einzelnen Klassen? 
-	 * Ja. Erst Typproblem lösen bevor enemy weiter bearbeitet wird
 	 *
 	 * Schau dir mal Java Enum an.  Oder du kannst ja ne methode machen die folgendes macht:
 	 * public int isEffective(Typ typ) return 1 wenn es ein effictiver Angriff war; return -1 wenn es gar nicht effictive war.
-	 * 
-	 * 
-	
 	 * 
 	 * @param typeAngriff
 	 * @param typDefender
 	 * @return
 	 *
+	 
 	public int isEffevtive(Typ typeAngriff, Typ typDefender){
 		
 		typeAngriff ist gleichstark wie typDefender
