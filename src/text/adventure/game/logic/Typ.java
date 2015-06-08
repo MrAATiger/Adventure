aaec768d989f39362dd06d2403956fb7cff7cd28
MrAATiger <alex.angeloussis@web.de>
2015-06-08 22:05:20 +0200

    add if statement for typ

--- src/text/adventure/game/logic/Typ.java
+++ src/text/adventure/game/logic/Typ.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 public enum Typ {
 	
-	Feuer, Wasser, Wind, Erde, Normal, Blitz;
+	Feuer, Wasser, Wind, Erde, Normal, Blitz, Pflanze;
 	
 	
 	public static final int SEHR_EFFEKTIV = 100;  
 	
-	public int istEffektiv (Typ typAngriff, Typ typVerteidigung1, Typ typVerteidigung2, Typ typVerteidigung3, Typ typVerteidigung4, Typ typVerteidigung5 ) {
+	public int istEffektiv (Typ typAngriff, Typ typVerteidigung1, Typ typVerteidigung2, Typ typVerteidigung3, Typ typVerteidigung4, Typ typVerteidigung5, Typ typVerteidigung6 ) {
 		//typeAngriff ist gleichstark wie typDefender
 		typAngriff = Typ.Feuer;
 		typVerteidigung1 = Typ.Wasser;
@@ -17,11 +17,20 @@ public enum Typ {
 		typVerteidigung3 = Typ.Erde;
 		typVerteidigung4 = Typ.Normal;
 		typVerteidigung5 = Typ.Blitz;
+		typVerteidigung6 = Typ.Pflanze;
+		
+		if (typAngriff == typVerteidigung2 || typAngriff == typVerteidigung4 || typAngriff == typVerteidigung5 || typAngriff == typVerteidigung3) {
 		return 0;
+		}
+		
+		else if (typAngriff == typVerteidigung6) {
 		//typeAngriff ist stärker als typDefender
 		return 1;
+		}
 		//typeAngriff ist schwächer als typDefender
+		else 
 		return -1;
+		}
 	}
 	
 	/*
