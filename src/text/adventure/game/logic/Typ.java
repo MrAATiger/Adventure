c585f22cf82018b11c4467e7142f173d4e561142
MrAATiger <alex.angeloussis@web.de>
2015-06-09 19:35:38 +0200

    add wasser typ

--- src/text/adventure/game/logic/Typ.java
+++ src/text/adventure/game/logic/Typ.java
@@ -9,7 +9,7 @@ public enum Typ {
 	
 	public static final int SEHR_EFFEKTIV = 100;  
 	
-	public int istEffektiv (Typ typAngriff, Typ typVerteidigung1, Typ typVerteidigung2, Typ typVerteidigung3, Typ typVerteidigung4, Typ typVerteidigung5, Typ typVerteidigung6 ) {
+	public int feuerIstEffektiv (Typ typAngriff, Typ typVerteidigung1, Typ typVerteidigung2, Typ typVerteidigung3, Typ typVerteidigung4, Typ typVerteidigung5, Typ typVerteidigung6 ) {
 		//typeAngriff ist gleichstark wie typDefender
 		typAngriff = Typ.Feuer;
 		typVerteidigung1 = Typ.Wasser;
@@ -31,28 +31,32 @@ public enum Typ {
 		else 
 		return -1;
 		}
+	
+
+public int wasserIstEffektiv (Typ typAngriff, Typ typVerteidigung1, Typ typVerteidigung2, Typ typVerteidigung3, Typ typVerteidigung4, Typ typVerteidigung5, Typ typVerteidigung6 ) {
+	//typeAngriff ist gleichstark wie typDefender
+	typAngriff = Typ.Wasser;
+	typVerteidigung1 = Typ.Feuer;
+	typVerteidigung2 = Typ.Wind;
+	typVerteidigung3 = Typ.Erde;
+	typVerteidigung4 = Typ.Normal;
+	typVerteidigung5 = Typ.Blitz;
+	typVerteidigung6 = Typ.Pflanze;
+	
+	if (typAngriff == typVerteidigung2 || typAngriff == typVerteidigung4 || typAngriff == typVerteidigung5 || typAngriff == typVerteidigung3) {
+	return 0;
 	}
 	
-	/*
-	 *
-	 * Schau dir mal Java Enum an.  Oder du kannst ja ne methode machen die folgendes macht:
-	 * public int isEffective(Typ typ) return 1 wenn es ein effictiver Angriff war; return -1 wenn es gar nicht effictive war.
-	 * 
-	 * @param typeAngriff
-	 * @param typDefender
-	 * @return
-	 *
-	 
-	public int isEffevtive(Typ typeAngriff, Typ typDefender){
-		
-		typeAngriff ist gleichstark wie typDefender
-		return 0;
-		typeAngriff ist stärker als typDefender
-		return 1;
-		typeAngriff ist schwächer als typDefender
-		return -1;
-		
+	else if (typAngriff == typVerteidigung1) {
+	//typeAngriff ist stärker als typDefender
+	return 1;
 	}
-	 */
-	
+	//typeAngriff ist schwächer als typDefender
+	else 
+	return -1;
+	}
 }
+	
+	
+	
+
